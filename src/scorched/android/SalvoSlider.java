a9bddfe9e0bbf1db2258b871b9a71e4ef02ef907
Colin McCabe <cmccabe@alumni.cmu.edu>
2009-03-11 13:31:35 -0700

    Improve slider graphics a lot

--- src/scorched/android/SalvoSlider.java
+++ src/scorched/android/SalvoSlider.java
@@ -9,6 +9,8 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RadialGradient;
 import android.graphics.Rect;
 import android.graphics.Shader;
 import android.graphics.drawable.Drawable;
@@ -22,27 +24,28 @@ import android.view.View;
  * A slider widget which the user can slide back and forth.
  * 
  * There are arrows on the ends for fine adjustments.
- *
- * Most functions are synchronized on mState to prevent wackiness.
- * The mState mutex should generally be held a pretty short amount of time.
+ * 
+ * Most functions are synchronized on mState to prevent wackiness. The mState
+ * mutex should generally be held a pretty short amount of time.
  */
 public class SalvoSlider extends View {
-    /*================= Types =================*/
+    /* ================= Types ================= */
     /** Describes the state of this slider */
     public enum SliderState {
-        /** The slider is not in use. 
-         * Only a blank space will be drawn.
-         * Touch will be disabled.
+        /**
+         * The slider is not in use. Only a blank space will be drawn. Touch
+         * will be disabled.
          */
         DISABLED,
 
-        /** The slider will be drawn using a bar graphic.
-         * Touch will be enabled.
+        /**
+         * The slider will be drawn using a bar graphic. Touch will be enabled.
          */
         BAR,
 
-        /** The slider will be drawn using the angle graphic.
-         * Touch will be enabled.
+        /**
+         * The slider will be drawn using the angle graphic. Touch will be
+         * enabled.
          */
         ANGLE,
     };
@@ -52,76 +55,85 @@ public class SalvoSlider extends View {
         void onPositionChange(int val);
     }
 
-    /*================= Constants =================*/
+    /* ================= Constants ================= */
     private static final String TAG = "SalvoSlider";
 
     private static final int BUTTON_PERCENT = 20;
-    
-    /*================= Members =================*/
+
+    /* ================= Members ================= */
     private SliderState mState;
 
-    ////// User input stuff
+    // //// User input stuff
     /** Listener to notify when slider value changes */
     private Listener mListener;
 
     /** Minimum slider value */
     public int mMin;
-    
+
     /** Maximum slider value. */
     public int mMax;
 
     /** Current slider value */
     private int mVal;
 
-    ////// Current configuration
-    /** Current slider color. If mColor == Color.WHITE then the slider is
-     * disabled. */
+    // //// Current configuration
+    /**
+     * Current slider color. If mColor == Color.WHITE then the slider is
+     * disabled.
+     */
     private int mColor;
 
     /** Current slider width */
     private int mWidth;
-    
+
     /** Current slider height */
     private int mHeight;
-    
-    ////// Things computed by cacheStuff()
+
+    // //// Things computed by cacheStuff()
     /** Current left boundary of slidable area */
     private int mLeftBound;
 
     /** Current right boundary of slidable area */
     private int mRightBound;
 
-    /** Shader for bar */
-    private Shader mBarShader;
-
     /** Gradient paint for bar */
-    private Paint mGradPaint;
+    private Paint mBarPaint;
 
-    ////// Temporaries
-    private Paint mTempPaint; 
+    /** Gradient paint for the top of buttons */
+    private Paint mButtonTopPaint;
+
+    /** Gradient paint for the bottom of buttons */
+    private Paint mButtonBottomPaint;
+
+    // //// Temporaries
+    private Paint mTempPaint;
 
     private Rect mTempRect;
 
-    /*================= Access =================*/
+    private Path mTempPath;
 
-    /*================= Operations =================*/
-    /** Cache a bunch of stuff that we don't want to have to recalculate on
-     * each draw().
+    /* ================= Access ================= */
+
+    /* ================= Operations ================= */
+    /**
+     * Cache a bunch of stuff that we don't want to have to recalculate on each
+     * draw().
      */
     private void cacheStuff() {
         assert Thread.holdsLock(mState);
         mLeftBound = (mWidth * BUTTON_PERCENT) / 100;
         mRightBound = (mWidth * (100 - BUTTON_PERCENT)) / 100;
 
-        int colors[] = new int[2];
+        int colors[] = new int[3];
         colors[0] = Color.WHITE;
         colors[1] = mColor;
-        mBarShader = new LinearGradient(0, 0, 0, mHeight,
-                            colors, null, Shader.TileMode.REPEAT);
-        mGradPaint = new Paint();
-        mGradPaint.setShader(mBarShader);
+        colors[2] = Color.WHITE;
+        Shader barShader = new LinearGradient(0, 0, 0, (mHeight * 3) / 2,
+                colors, null, Shader.TileMode.REPEAT);
+        mBarPaint = new Paint();
+        mBarPaint.setShader(barShader);
     }
-    
+
     @Override
     protected void onDraw(Canvas canvas) {
         synchronized (mState) {
@@ -136,15 +148,20 @@ public class SalvoSlider extends View {
             case BAR:
                 drawEndButtons(canvas);
 
-                int x = mLeftBound + 
-                    (((mRightBound - mLeftBound) * mVal) / 
-                        Math.abs(mMax - mMin));
+                int x = mLeftBound
+                        + (((mRightBound - mLeftBound) * mVal) / Math.abs(mMax
+                                - mMin));
                 mTempRect.set(mLeftBound, 0, x, mHeight);
-                canvas.drawRect(mTempRect, mGradPaint);
+                canvas.drawRect(mTempRect, mBarPaint);
                 mTempPaint.setColor(Color.BLACK);
                 mTempRect.set(mLeftBound, mHeight + 1, x, mRightBound);
                 canvas.drawRect(mTempRect, mTempPaint);
                 break;
+
+            // public void drawText(String text,
+            // float x,
+            // float y,
+            // Paint paint)
 
             case ANGLE:
                 drawEndButtons(canvas);
@@ -159,13 +176,29 @@ public class SalvoSlider extends View {
 
     private void drawEndButtons(Canvas canvas) {
         // Draw end buttons
-        mTempPaint.setColor(Color.GRAY);
-        mTempRect.set(0, 0, mLeftBound, mHeight);
+        drawEndButton(canvas, 0, mLeftBound);
+        drawEndButton(canvas, mWidth, mRightBound);
+    }
+
+    private void drawEndButton(Canvas canvas, int xLeft, int xRight) {
+        mTempPaint.setColor(Color.BLACK);
+        mTempRect.set(xLeft, 0, xRight, mHeight);
         canvas.drawRect(mTempRect, mTempPaint);
 
-        mTempPaint.setColor(Color.CYAN);
-        mTempRect.set(mRightBound, 0, mWidth, mHeight);
-        canvas.drawRect(mTempRect, mTempPaint);
+        mTempPaint.setARGB(255, 236, 189, 62);
+        mTempPaint.setStrokeWidth(mHeight / 20);
+        canvas.drawLine(xLeft, 0, xRight, 0, mTempPaint);
+        canvas.drawLine(xLeft, mHeight, xRight, mHeight, mTempPaint);
+        canvas.drawLine(xLeft, 0, xLeft, mHeight, mTempPaint);
+        canvas.drawLine(xRight, 0, xRight, mHeight, mTempPaint);
+
+        int w = xRight - xLeft;
+        mTempPath.moveTo(xLeft + (w / 5), mHeight / 2);
+        mTempPath.lineTo(xLeft + ((4 * w) / 5), mHeight / 5);
+        mTempPath.lineTo(xLeft + ((4 * w) / 5), (mHeight * 4) / 5);
+        mTempPaint.setStrokeWidth(1);
+        canvas.drawPath(mTempPath, mTempPaint);
+        mTempPath.rewind();
     }
 
     @Override
@@ -174,32 +207,29 @@ public class SalvoSlider extends View {
             if (mState == SliderState.DISABLED) {
                 return true;
             }
-            /*int action = me.getAction();
-            if ((action == MotionEvent.ACTION_DOWN) ||
-                (action == MotionEvent.ACTION_MOVE) ||
-                (action == MotionEvent.ACTION_UP)) 
-            {
-                int newVal = me.getX();
-            }*/
+            /*
+             * int action = me.getAction(); if ((action ==
+             * MotionEvent.ACTION_DOWN) || (action == MotionEvent.ACTION_MOVE)
+             * || (action == MotionEvent.ACTION_UP)) { int newVal = me.getX(); }
+             */
             return true;
         }
     }
 
-    /** Change the slider state to something else.
-     *  This can be called from non-UI threads. */
-    public void setState(SliderState state, 
-    					 Listener listener,
-                         int min, int max, int val, int color)
-    {
-    	Log.w(TAG, "setState state=" + state + 
-    			",min=" + min + ",max=" + max +
-    			",color=" + color);
+    /**
+     * Change the slider state to something else. This can be called from non-UI
+     * threads.
+     */
+    public void setState(SliderState state, Listener listener, int min,
+            int max, int val, int color) {
+        Log.w(TAG, "setState state=" + state + ",min=" + min + ",max=" + max
+                + ",color=" + color);
         synchronized (mState) {
             mState = state;
             mListener = listener;
 
             // user input stuff
-            assert(mListener != null);
+            assert (mListener != null);
             mMin = min;
             mMax = max;
             mVal = val;
@@ -214,7 +244,7 @@ public class SalvoSlider extends View {
         postInvalidate();
     }
 
-    /*================= Lifecycle =================*/
+    /* ================= Lifecycle ================= */
     private void construct() {
         mState = SliderState.DISABLED;
         setFocusable(true);
@@ -222,6 +252,7 @@ public class SalvoSlider extends View {
         // Temporaries
         mTempPaint = new Paint();
         mTempRect = new Rect();
+        mTempPath = new Path();
     }
 
     /** Constructor for "manual" instantiation */
@@ -229,7 +260,7 @@ public class SalvoSlider extends View {
         super(context);
         construct();
     }
-    
+
     /** Contructor for layout file */
     public SalvoSlider(Context context, AttributeSet attrs) {
         super(context, attrs);
