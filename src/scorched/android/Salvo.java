f3719eed335f4aeb649d5ae13e46c754ee7f29c3
Colin McCabe <cmccabe@alumni.cmu.edu>
2009-03-12 20:38:18 -0700

    Restructure app based around 3 activities
    Restructure the app in terms of a title screen, a GameSetup screen, and a
    RunGame screen. Each screen is its own activity. Each activity has its own
    layout.

--- src/scorched/android/Salvo.java
+++ src/scorched/android/Salvo.java
@@ -1,11 +1,13 @@
 package scorched.android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ZoomButton;
 
 import scorched.android.Model;
@@ -16,9 +18,6 @@ public class Salvo extends Activity {
     private static final String TAG = "Salvo";
 
     /*================= Data =================*/
-    private GameControlView mGameControl;
-    private Model mModel;
-    private Graphics mGraphics;
 
     /*================= Utility =================*/
 
@@ -26,76 +25,40 @@ public class Salvo extends Activity {
     /**
      * Invoked when the Activity is created.
      *
-     * @param savedInstanceState a Bundle containing state saved from a previous
-     *        execution, or null if this is a new execution
+     * @param savedInstanceState a Bundle containing state saved from a 
+     *        previous execution, or null if this is a new execution
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Player players[] = new Player[5];
-        players[0] = new LocalHumanPlayer(0);
-        players[1] = new ComputerPlayer(1);
-        players[2] = new ComputerPlayer(2);
-        players[3] = new ComputerPlayer(3);
-        players[4] = new LocalHumanPlayer(4);
-        mModel = new Model(players);
-        mGraphics = new Graphics(getBaseContext(), mModel);
-
         ////////////////// setContentView
-        requestWindowFeature(Window.FEATURE_NO_TITLE); // turn off title bar
         Log.w(TAG, "setContentView");
-        setContentView(R.layout.main);
+        setContentView(R.layout.title);
 
         ////////////////// Get pointers to stuff
-        mGameControl = (GameControlView)findViewById(R.id.scorched_layout);
-        final SalvoSlider powerSlider =
-            (SalvoSlider)findViewById(R.id.PowerSlider);
-        final SalvoSlider angleSlider =
-            (SalvoSlider)findViewById(R.id.AngleSlider);
-        final Button fireButton = (Button)findViewById(R.id.FireButton);
-        final ZoomButton zoomIn = (ZoomButton)findViewById(R.id.ZoomIn);
-        final ZoomButton zoomOut = (ZoomButton)findViewById(R.id.ZoomOut);
+        final ImageView myImage = (ImageView)findViewById(R.id.title_picture);
+        final Button newGame = (Button)findViewById(R.id.new_game);
+        final Button about = (Button)findViewById(R.id.about);
+        final Button buyGame = (Button)findViewById(R.id.buy_game);
+        final Button help = (Button)findViewById(R.id.help);
 
         ////////////////// Initialize stuff
-        mGameControl.initialize(mModel, mGraphics, powerSlider, angleSlider);
-        fireButton.setOnClickListener(new OnClickListener() {
-            public void onClick(View arg0) {
-                mGameControl.getThread().onFireButton();
-            }
-        });
-        zoomIn.setOnClickListener(new OnClickListener() {
-            public void onClick(View arg0) {
-                mGameControl.getThread().onZoomIn();
-            }
-        });
-        zoomOut.setOnClickListener(new OnClickListener() {
-            public void onClick(View arg0) {
-                mGameControl.getThread().onZoomOut();
+        final Activity titleActivity = this;
+        newGame.setOnClickListener(new OnClickListener() {
+        	public void onClick(View arg0) {
+            	Intent setupIntent =
+            		new Intent().setClass(titleActivity, GameSetupActivity.class);
+            	    startActivity(setupIntent); 
             }
         });
     }
 
-    /**
-     * Invoked when the Activity loses user focus.
-     */
     @Override
     protected void onPause() {
-        //super.onPause();
-        //mGameControl.getThread().pause(); // pause game when Activity pauses
+    	super.onPause();
     }
 
-    /**
-     * Notification that something is about to happen, to give the Activity a
-     * chance to save state.
-     *
-     * @param outState a Bundle into which this Activity should save its state
-     */
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        // just have the View's thread save its state into our Bundle
-        super.onSaveInstanceState(outState);
-        mGameControl.getThread().saveState(outState);
-        Log.w(this.getClass().getName(), "onSaveInstanceState called");
-    }
+    protected void onSaveInstanceState(Bundle outState) { }
 }
