337913837a43f657291f25876d165d0d96337e9d
Colin McCabe <cmccabe@alumni.cmu.edu>
2009-04-14 00:34:49 -0700

    Upgrade AutoPack to handle arrays

--- src/scorched/android/AutoPack.java
+++ src/scorched/android/AutoPack.java
@@ -5,10 +5,34 @@ import java.lang.reflect.Field;
 import android.os.Bundle;
 
 /**
- * Handles automatic serialization / deserialization.
+ * Automatically packs class data into Android Bundles (also unpacks).
+ *
+ * Most classes have a set of core data that they want to preserve across
+ * changes in screen orientation, etc.
+ * Android requires you to write this data to a Bundle in
+ * onSaveInstanceState(), and be ready to re-create the object from this
+ * bundle in onCreate().
+ *
+ * It is painful and error-prone to write these serialization /
+ * deserialization functions by hand.
+ * AutoPack provides a better way.
+ *
+ * Simply move your class's "essential" data into a static inner class. All
+ * the members of this class should be public, and it should contain only
+ * "simple" datatypes that can be handled natively by the Bundle methods.
+ * Then, you can call autoPack() in onSaveInstanceState, and autoUnpack in
+ * onCreate(), and rest assured that your data will be properly recreated.
+ *
+ * One small additional detail: if you are saving multiple instances of a
+ * class (for example, if you have 5 Players who all want to save their
+ * data), you must provide a "prefix" string argument to separate out each
+ * instance.
  */
 public abstract class AutoPack
 {
+    /*================= Constant =================*/
+    public static final String EMPTY_STRING = "";
+
     /*================= Utility =================*/
     private static String fieldNameToKey(String prefix, String fieldName) {
         StringBuilder b = new StringBuilder(80);
@@ -17,9 +41,10 @@ public abstract class AutoPack
     }
 
     /*================= Operations =================*/
-    /** Add all elements of obj to bundle b.
+    /** Add all elements of obj to bundle "map".
      *
-     * Their names will be just as they were in the class.
+     * Their names will be just as they were in the class, except that
+     * "prefix" will be prepended.
      */
     @SuppressWarnings("unchecked")
     public static void autoPack(Bundle map, String prefix, Object obj) {
@@ -31,6 +56,27 @@ public abstract class AutoPack
                 if (fc.isEnum()) {
                     Enum myEnum = (Enum)f.get(obj);
                     map.putInt(name, myEnum.ordinal());
+                }
+                else if (fc.isArray()) {
+                    Class ec = fc.getComponentType();
+                    if (ec == boolean.class)
+                        map.putBooleanArray(name, (boolean[])f.get(obj));
+                    else if (ec == short.class)
+                        map.putShortArray(name, (short[])f.get(obj));
+                    else if (ec == int.class)
+                        map.putIntArray(name, (int[])f.get(obj));
+                    else if (ec == long.class)
+                        map.putLongArray(name, (long[])f.get(obj));
+                    else if (ec == float.class)
+                        map.putFloatArray(name, (float[])f.get(obj));
+                    else if (ec == double.class)
+                        map.putDoubleArray(name, (double[])f.get(obj));
+                    else if (ec == String.class)
+                        map.putStringArray(name, (String[])f.get(obj));
+                    else {
+                        throw new RuntimeException("autoPack doesn't know " +
+                            "how to pack fields of type array of " + ec);
+                    }
                 }
                 else if (fc == oc) {
                     // Ignore the "this" field I wish I could be absolutely
@@ -59,9 +105,9 @@ public abstract class AutoPack
                 }
             }
             catch (java.lang.IllegalAccessException e) {
-                // This code is unreachable, as you can easily see.
-                // However, the compiler's reasoning powers are limited,
-                // so we have to go through the formalities here.
+                // This code should be unreachable.
+                // If you ever reach it, make sure that all of your class'
+                // members are public.
                 throw new RuntimeException(e.toString());
             }
         }
@@ -83,8 +129,9 @@ public abstract class AutoPack
             StringBuilder b = new StringBuilder(200);
             b.append("error calling newInstance:").append(e.toString());
             b.append(" ");
-            b.append("Please check that you have provided a default constructor ");
-            b.append("for the class you are using with AutoPack.");
+            b.append("Please check that you have provided a default ");
+            b.append("constructor for the class you are ");
+            b.append("using with AutoPack.");
             throw new RuntimeException(b.toString());
         }
         for (Field f : oc.getDeclaredFields()) {
@@ -94,6 +141,28 @@ public abstract class AutoPack
                 if (fc.isEnum()) {
                     Object vals[] = fc.getEnumConstants();
                     f.set(ret, vals[map.getInt(name)]);
+                }
+                else if (fc.isArray()) {
+                    Class ec = fc.getComponentType();
+                    if (ec == boolean.class)
+                        f.set(ret, map.getBooleanArray(name));
+                    else if (ec == short.class)
+                        f.set(ret, map.getShortArray(name));
+                    else if (ec == int.class)
+                        f.set(ret, map.getIntArray(name));
+                    else if (ec == long.class)
+                        f.set(ret, map.getLongArray(name));
+                    else if (ec == float.class)
+                        f.set(ret, map.getFloatArray(name));
+                    else if (ec == double.class)
+                        f.set(ret, map.getDoubleArray(name));
+                    else if (ec == String.class)
+                        f.set(ret, map.getStringArray(name));
+                    else {
+                        throw new RuntimeException("autoUnpack doesn't " +
+                            "know how to unpack fields of type " +
+                            "array of " + ec);
+                    }
                 }
                 else if (fc == oc) {
                     // Ignore the "this" field. As described in autoPack().
@@ -118,7 +187,9 @@ public abstract class AutoPack
                 }
             }
             catch (java.lang.IllegalAccessException e) {
-                // As in pack()
+                // This code should be unreachable.
+                // If you ever reach it, make sure that all of your class's
+                // members are public.
                 throw new RuntimeException(e.toString());
             }
         }
