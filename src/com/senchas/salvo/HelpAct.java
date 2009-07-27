74f746273964948d82373d160ec55b838d9db63a
Colin McCabe <cmccabe@alumni.cmu.edu>
2009-07-26 19:06:26 -0700

    Fix tabbing of HelpAct.java

--- src/com/senchas/salvo/HelpAct.java
+++ src/com/senchas/salvo/HelpAct.java
@@ -21,7 +21,7 @@ public class HelpAct extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
